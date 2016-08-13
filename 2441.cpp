271d220c6ab34974aa0c7c2bc3d897ce7afa403f
help2r <help2r>
2016-08-13 19:18:06 +0900

    .

---
+++ 2441.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	for (int j = 0; j < n; j++)
+	{
+		for (int i = 0; i < j; i++)
+			cout << " ";
+		for (int i = 0; i < n - j; i++)
+			cout << "*";
+		cout << endl;
+	}
+	return 0;
+}
